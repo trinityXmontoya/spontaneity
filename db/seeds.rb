1262ce778806682edfb1ff59634c7f8850c31ec7
trinityXmontoya <trinityXmontoya@gmail.com>
2014-09-01 17:18:04 -0400

    add initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 Destination.create(id: 1, name: "mountain", lat: 42.3485, long: 71.0826)
 Destination.create(id: 2, name: "hill", lat: 42.3529, long: 71.1321)
 Destination.create(id: 3, name: "beach", lat: 40.6892, long:74.0444)
-Destination.create(id: 4, name: "river", lat: 40.7506, long: 73.9936)
-Adventure.create(destination_id: 1, user_id: 1, zipcode: 02135, time_limit: 30)
-User.create(id: 1, email: "trinity@gmail.com")
+user1= User.create(id: 1, email: "john@gmail.com")
+user1.adventures.create(destination_id: 1, start_location: "02135", time_limit: 30, status: 'completed')
+user1.adventures.create(destination_id: 2, start_location: "02135", time_limit: 30, status: 'completed')
+user1.adventures.create(destination_id: 3, start_location: "02135", time_limit: 30, status: 'in-progress')
